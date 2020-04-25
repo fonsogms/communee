400a726582e12e79eac7216d64f283695d2a89f9
Alfonso García-Miñaur <Alfonso@Alfonso-GMS.local>
2020-04-25 12:24:51 +0200

    testing .env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,9 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 function App() {
+  console.log(process.env);
+  console.log(process.env.REACT_APP_SERVER_URL);
+  console.log(process.env.REACT_APP_SERVER_HELLO);
   return (
     <div className="App">
       <Navbar></Navbar>
---
+++ client/src/components/Mapbox.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactMapGL from "react-map-gl";
+export default class Mapbox extends Component {
+  render() {
+    return (
+      <div>
+        <ReactMapGL></ReactMapGL>
+      </div>
+    );
+  }
+}
